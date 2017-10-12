433f10023667ccba933d90d6a525ff0c78e37271
Bonaparte ITUZE <ibonaparte4@gmail.com>
2017-10-12 08:46:39 +0200

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace DMM.HEHE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(550, 53);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Querry Bar";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(444, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(408, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(550, 290);
+            this.listBox1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 159);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(550, 296);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Logs";
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.listView1.Location = new System.Drawing.Point(0, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(550, 288);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.Visible = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 57);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(550, 96);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Allowed Queries";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(21, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "create table table_name columns(\"col1\",\"col2\",\"col3\")";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(21, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 42);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "insert into table_name values(\"col1\",\"col2\",\"col3\")";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "drop table table_name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "select * from table_name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 498);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "iDBMS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+namespace DMM.HEHE
+{
+    public partial class Form1 : Form
+    {
+        private Hashtable Database = new Hashtable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string querry = textBox1.Text.ToLower();
+            string[] querry_identifier = querry.Split(' ');    
+              
+            if(querry_identifier[0]=="create" && querry_identifier[1]=="table" && querry_identifier[2] != " " && querry_identifier[3] != " ")
+            {
+                string result = "creating table " + querry_identifier[2] + " with columns ";
+                string[] column = querry_identifier[3].Split('"');
+                string column_count = null;
+                for(int i=0;i<column.Count();i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        if(i < column.Count()-1)
+                            column_count = column_count + column[i] + " ";
+                        
+                        //result = result + "[" + column[i] + "]";
+                    }
+                }
+                string[] columns = column_count.Split(' ');
+                createTable(querry_identifier[2], columns);
+               // MessageBox.Show(result);
+            }
+            else 
+            if(querry_identifier[0] == "drop" && querry_identifier[1]== "table" )
+            {
+                if (table_exists(querry_identifier[2]))
+                {
+                    //MessageBox.Show("Deleting Table");
+                    listBox1.Items.Add("Table " + querry_identifier[2]+" Deleted");
+                    dropTable(querry_identifier[2]);
+                }
+                else
+                {
+                    //MessageBox.Show("Table not found");
+                    listBox1.Items.Add("table " + querry_identifier[2] + " not found");
+                }
+            }
+            else
+            if (querry_identifier[0] == "select" && querry_identifier[1] == "*" && querry_identifier[2] == "from" && querry_identifier[3] != " ")
+            {
+                if (table_exists(querry_identifier[3]))
+                {                   
+                    if(getTable(querry_identifier[3]))
+                    {
+                        listBox1.Items.Add("Getting Content from " + querry_identifier[3]);
+                    }
+                }
+                else
+                {
+                    listBox1.Items.Add("table " + querry_identifier[3] + " not found");
+                }
+            }
+            else
+            if (querry_identifier[0] == "insert" && querry_identifier[1] == "into" && querry_identifier[2] != " ")
+            {
+                if (table_exists(querry_identifier[2]))
+                {
+                    string[] values = querry_identifier[3].Split('"');
+                    listBox1.Items.Add("Added new row to:  " + querry_identifier[2]);
+                    string column_count = null;
+                    for (int i = 0; i < values.Count(); i++)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            if (i < values.Count() - 1)
+                                column_count = column_count + values[i] + " ";
+                        }
+                    }
+                    string[] value = column_count.Split(' ');
+                    insertTable(querry_identifier[2], value);
+
+                }
+                else
+                {
+                    listBox1.Items.Add("table " + querry_identifier[2] + " not found");
+                }
+            }
+            else
+            {
+                listBox1.Items.Add("Unrecognized Querry");
+            }
+        }       
+        private void createTable(string table_name, string[] columns)
+        {
+            if (!table_exists(table_name))
+            {
+                DeactivateList(listView1);
+                List<List<string>> table = new List<List<string>>();
+                List<string> row = columns.ToList<string>();
+                table.Add(row);
+                Database.Add(table_name, table);
+                listBox1.Items.Add("table " + table_name + " created");
+                //O(1)
+            }
+            else
+            {
+                MessageBox.Show("Table already exists");
+            }
+        }      
+        private bool table_exists(string table_name)
+        {
+            if (Database.ContainsKey(table_name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool dropTable(string table_name)
+        {
+            DeactivateList(listView1);
+            if (table_exists(table_name))
+            {
+                Database.Remove(table_name);
+                return true;
+            }else
+            {
+                return false;
+            }
+            //O(1)
+        }
+        private bool getTable(string table_name)
+        {
+            if (table_exists(table_name))
+            {
+                activateList(listView1);
+                List<List<string>> table_values = Database[table_name] as List<List<string>>;
+
+                for(int i=0; i<table_values[0].Count();i++)
+                {
+                    listView1.Columns.Add(table_values[0][i]);
+                }
+
+                for(int j=1;j<table_values.Count();j++)
+                {
+                    string[] row = table_values[j].Select(i => i.ToString()).ToArray();
+                    var listViewItem = new ListViewItem(row);
+                    listView1.Items.Add(listViewItem);
+                }
+                return true;
+            }
+            else
+                return false;
+
+            //O(n*n)
+        }
+        private bool insertTable(string table_name, string[] values)
+        {
+            DeactivateList(listView1);
+            listBox1.Visible = true;
+            if (table_exists(table_name))
+            {
+                listBox1.Visible = true;
+                List<List<string>> table_values = Database[table_name] as List<List<string>>;
+                List<string> new_row = values.ToList();
+                table_values.Add(new_row);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            //O(n)
+        }
+
+        private bool DeactivateList(ListView list)
+        {
+            listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            listView1.Items.Clear();
+            listView1.Visible = false;
+            listBox1.Visible = true;
+            return true;
+        }
+
+        private bool activateList(ListView list)
+        {
+            listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Clickable;
+            listView1.Items.Clear();
+            listView1.Visible = true;
+            listBox1.Visible = false;
+            return true;
+        }
+    }
+}
